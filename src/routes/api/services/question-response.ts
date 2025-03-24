bfece27e38bcd69b14ba956363cb7f9abc6a2e20
Dattatraya-Londhe <londhedn@gmail.com>
2025-03-24 18:29:15 +0530

    Updated form submission load functionality.

--- src/routes/api/services/question-response.ts
+++ src/routes/api/services/question-response.ts
@@ -1,5 +1,5 @@
 import { BACKEND_API_URL } from "$env/static/private";
-import { post_ } from "./common";
+import { get_, post_ } from "./common";
 
 export const  createQuestionResponse = async (
 	FormSubmissionId: string,
@@ -12,4 +12,29 @@ export const  createQuestionResponse = async (
 
 	const url = BACKEND_API_URL + `/question-responses/save`;
 	return await post_(url, body);
+};
+
+export const searchquestionResponse = async (
+    searchParams?: any
+) => {
+    console.log('Search params are ', searchParams);
+    let searchString = '';
+    if (searchParams) {
+        const keys = Object.keys(searchParams);
+        if (keys.length > 0) {
+            searchString = '?';
+            const params = [];
+            for (const key of keys) {
+                if (searchParams[key]) {
+                    const param = `${key}=${searchParams[key]}`;
+                    params.push(param);
+                }
+            }
+            searchString += params.join('&');
+        }
+    }
+    console.log('Search string is ', searchString);
+    const url = BACKEND_API_URL + `/question-responses/search${searchString}`;
+    const res = await get_(url);
+    return res;
 };
--- src/routes/form/submission/[id]/+page.server.ts
+++ src/routes/form/submission/[id]/+page.server.ts
@@ -1,32 +1,69 @@
 import { error, type ServerLoadEvent } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { getFormTemplateDetails } from "../../../api/services/form-template";
-import { getFormSubmission } from "../../../api/services/submission";
+import { getFormSubmission, searchFormSubmission } from "../../../api/services/submission";
+import { searchquestionResponse } from "../../../api/services/question-response";
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 export const load: PageServerLoad = async (event: ServerLoadEvent) => {
-
+    console.log('Inside form submission LOAD..........');
     event.depends('app:allNodes')
-    const submissionId = event.params.id;
-    try {
-        const submission = await getFormSubmission(submissionId);
-        // console.log(submission.Data.ParentFormTemplate.id, "this is submission");
+    const submissionKey = event.params.id;
+    console.log('Form submission id is ', submissionKey);
 
-        const templateId = submission.Data.ParentFormTemplate.id
-        const response = await getFormTemplateDetails(templateId);
-        // console.log(response, "this is template");
-
-        if (response.Status === 'failure' || response.HttpCode !== 200) {
-            throw error(response.HttpCode, response.Message);
-        }
-
-        const assessmentTemplate = response.Data;
-        return {
-            assessmentTemplate,
-            message: response.Message
-        };
-    } catch (error) {
-        console.error(`Error retriving assessment templates: ${error}`);
+    if (!submissionKey) {
+        error(400, 'Invalid submission link!');
     }
+
+    const submissionRecord = await searchFormSubmission({
+        encrypted: submissionKey
+    });
+
+    console.log('Submission record is ', submissionRecord);
+    if (!submissionRecord && submissionRecord?.Status === 'failure' && submissionRecord?.HttpCode !== 200 ) {
+        error(400, 'Invalid submission link!');
+    }
+    
+    if (submissionRecord.Data?.RetrievedCount !== 1) {
+        error(400, 'Invalid submission link!');
+    }
+
+    const submission = submissionRecord.Data?.Items[0];
+    console.log('Submission record details ', submission);
+    console.log('Template id ', submission.FormTemplateId);
+
+    const submissionStatus = submission?.Status || 'LinkShared';
+
+    const today = new Date();
+    if (!submission.SubmittedAt && new Date(submission.ValidTill) < today) {
+        error(410, 'Submission link has expired!');
+    }
+        
+    const templateId = submission.FormTemplateId;
+
+    const response = await getFormTemplateDetails(templateId);
+
+    if (response.Status === 'failure' || response.HttpCode !== 200) {
+        error(response.HttpCode, response.Message);
+    }
+    const assessmentTemplate = response.Data;
+
+    console.log('Assessment template details ', assessmentTemplate);
+    console.log('Submission status ', submissionStatus);
+    let questionResponse = null;
+    if (submissionStatus === 'LinkShared') {
+        console.log('Submission is saved');
+        questionResponse = await searchquestionResponse({ formSubmissionId: submission.Id });
+        console.log('Question response ', questionResponse);
+        questionResponse = questionResponse.Data?.Items || [];
+    }
+
+    return {
+        assessmentTemplate,
+        message: response.Message,
+        questionResponse,
+        submissionStatus
+    };
+   
 };
