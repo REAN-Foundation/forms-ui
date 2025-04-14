a4e9970564a8f94e3e1193f146a4ef88cecdff51
inflection-prashant <prashant.kharade@inflectionzone.com>
2025-04-14 16:10:57 +0530

    "Preview of common added components"

---
+++ src/lib/components/preview/common/index.ts
@@ -0,0 +1,7 @@
+export { default as AddressPreview } from './AddressPreview.svelte';
+export { default as AgePreview } from './AgePreview.svelte';
+export { default as BirthDatePreview } from './BirthDatePreview.svelte';
+export { default as EmailPreview } from './EmailPreview.svelte';
+export { default as GenderPreview } from './GenderPreview.svelte';
+export { default as NamePreview } from './NamePreview.svelte';
+export { default as PhoneNumberPreview } from './PhoneNumberPreview.svelte';
