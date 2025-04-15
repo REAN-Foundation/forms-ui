a4abe92426c130c11f05c53df659a44dd19fa27a
inflection-prashant <prashant.kharade@inflectionzone.com>
2025-04-15 10:52:37 +0530

    "Common plugins "

--- src/lib/components/forms/responseType/common/index.ts
+++ src/lib/components/forms/responseType/common/index.ts
@@ -1,3 +1,7 @@
-export { default as PhoneNumberForm } from './PhoneNumberFieldEditor.svelte';
+export { default as NameForm } from './NameFieldEditor.svelte';
 export { default as AgeForm } from './AgeFieldEditor.svelte';
+export { default as BirthDateForm } from './BirthDateFieldEditor.svelte';
+export { default as PhoneNumberForm } from './PhoneNumberFieldEditor.svelte';
+export { default as GenderForm } from './GenderFieldEditor.svelte';
 export { default as EmailForm } from './EmailFieldEditor.svelte';
+export { default as AddressForm } from './AddressFieldEditor.svelte';
